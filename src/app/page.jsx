c0684bcfcf87182d08dda5aa2a61fd5278c96b10
LuisMZampar <rm550531@fiap.com.br>
2023-10-11 21:36:23 -0300

    Refatorando o projeto base.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
       <h1>Home - RM550531</h1>
 
-      <ul>
-        <li>
-          <Link href="/post">POST</Link>
-        </li>
-        <li>
-          <Link href="/post/post1">POST-1</Link>
-        </li>
-        <li>
-          <Link href="/post/post2">POST-1</Link>
-        </li>
-      </ul>
     </div>
   );
 }
